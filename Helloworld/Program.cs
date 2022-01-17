6e35f093b553c0073847a7267b5e0e47f70d9bfa
aum1110 <aumsohoni1110@gmail.com>
2022-01-17 15:09:45 +0530

    Initial commit

---
+++ Helloworld/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Title = "Skynet";
+            Console.ForegroundColor = ConsoleColor.Green;
+
+
+            Console.WriteLine("Hello World");
+
+
+
+
+
+
+
+
+         
+            /*
+            Console.WriteLine("There was a guy named ......");
+            Console.ReadLine();
+
+            Console.WriteLine("...... He used to love  his bicycle \n one day his bicycle got stolen and he started........");
+            Console.ReadLine();
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("he got kidnapped");*/
+
+
+
+
+        }
+    }
+}
