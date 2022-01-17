eb4fbdb912cfbcc80027f332cb03bf30ec5329ca
aum1110 <aumsohoni1110@gmail.com>
2022-01-17 15:14:47 +0530

    Update Program.cs

--- Helloworld/Program.cs
+++ Helloworld/Program.cs
@@ -21,18 +21,19 @@ namespace Helloworld
 
 
          
-            /*
-            Console.WriteLine("There was a guy named ......");
+            /* STORY PART */
+            Console.WriteLine("There was a Boy named ......");
             Console.ReadLine();
 
             Console.WriteLine("...... He used to love  his bicycle \n one day his bicycle got stolen and he started........");
             Console.ReadLine();
 
-            Console.ForegroundColor = ConsoleColor.DarkYellow;
-            Console.WriteLine("he got kidnapped");*/
 
+            Console.WriteLine("........ Suddenly a Man came and \n He.......");
+            Console.ReadLine();
 
-
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Boy got Afraid and ran");
 
         }
     }
