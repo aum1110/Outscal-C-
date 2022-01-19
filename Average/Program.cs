87ed4c159c604ae130f4e4326819cfb1882cb188
aum1110 <aumsohoni1110@gmail.com>
2022-01-19 16:42:42 +0530

    Average Code

---
+++ Average/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Average
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int avg;
+
+            Console.WriteLine("Average Of Three Numbers");
+
+            Console.WriteLine("Enter the First Number :");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the Second Number :");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the Third Number :");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            avg = a + b + c / 3;
+
+            Console.WriteLine("The Average of Three Number is :" + avg);
+
+
+
+        }
+    }
+}
