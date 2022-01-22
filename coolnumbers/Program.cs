2eeb11d8f4a4a59724e292f4aaf560c67be19c3e
aum1110 <aumsohoni1110@gmail.com>
2022-01-22 16:38:22 +0530

    loops

---
+++ coolnumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace coolnumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            Console.WriteLine("Enter till what MAx. amnt you want cool numbers : ");
+            int count = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= count; i++)
+            {
+                double result = Math.Pow(2 , i);
+                Console.WriteLine(result);
+                
+            
+            }
+
+        }
+    }
+}
---
+++ loops/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random number = new Random();
+
+            double roll1= 0;
+            double roll2 = 0;
+            int attempts = 0;
+            Console.WriteLine(" Press Enter To Roll The Dice : ");
+            while (roll1!= 6 && roll2 != 6)
+                {
+               
+                    Console.ReadKey();
+                    roll1 = number.Next(1, 7);
+                    roll2 = number.Next(1, 7);
+                    Console.WriteLine("Roll1 Number is : " + roll1);
+                    Console.WriteLine("Roll2 Number is : " + roll2 );
+                    attempts++;
+                }
+                
+
+            Console.WriteLine("Number of Attempts took : " + attempts);
+
+
+        }
+    }
+}
