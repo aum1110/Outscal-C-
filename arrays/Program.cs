c71a6540561eb9bd4c1eca2f354d36b6ef918d3b
aum1110 <aumsohoni1110@gmail.com>
2022-01-24 18:13:22 +0530

    arrays

---
+++ arrays/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] movies = new string[4];
+
+            Console.WriteLine("Enter 4 Movies name : ");
+
+            for (int i = 0; i <= movies.Length; i++)
+            {
+                movies[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("\n Here they are in Alphabetically:");
+
+            Array.Sort(movies);
+
+            for (int i = 0; i <= movies.Length; i++)
+                {
+
+                Console.WriteLine(movies[i]);
+            
+            
+                }
+
+
+
+
+
+
+
+
+        }
+    }
+}
