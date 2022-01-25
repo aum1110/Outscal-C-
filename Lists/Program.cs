7eaae985923aea17f50562974bd0903cc71dea14
aum1110 <aumsohoni1110@gmail.com>
2022-01-25 10:13:32 +0530

    Lists

---
+++ Lists/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<String> shoppinglist = new List<string>();
+
+            shoppinglist.Add("Dreams");
+            shoppinglist.Add("Games");
+            shoppinglist.Add("PS5");
+            shoppinglist.Add("XboX");
+
+
+            for (int i = 0; i< shoppinglist.Count; i++)
+            {
+                Console.WriteLine(shoppinglist[i]);
+            }
+
+            shoppinglist.Remove("Games" 
+                );
+            shoppinglist.RemoveAt(2);
+
+
+            Console.WriteLine("---------------------------------------");
+
+
+            for (int i = 0; i < shoppinglist.Count; i++)
+            {
+                Console.WriteLine(shoppinglist[i]);
+            }
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
