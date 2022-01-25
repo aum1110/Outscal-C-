8a09d63859bf27de65c4001bcb778dcefaf16e29
aum1110 <aumsohoni1110@gmail.com>
2022-01-25 15:55:10 +0530

    MethodAS2

--- MethodAs2/Program.cs
+++ MethodAs2/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 
-namespace MethodAs2
+namespace Methodas1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter three numbers for multiplication : ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            int answer = Multiply(a, b, c);
+
+            Console.WriteLine(answer);
+            
+
+
+
         }
+
+        static int Multiply(int a, int b, int c)
+        {
+
+            int result = a * b * c;
+            return result;
+
+        }
+
+
     }
 }
