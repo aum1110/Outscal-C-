84872e73aca6345653cf96d04fcf36ab86a591c1
aum1110 <aumsohoni1110@gmail.com>
2022-01-25 15:10:44 +0530

    MethodAssignment1

---
+++ Methodas1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Methodas1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter three numbers for multiplication : ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            Multiply(a, b, c);
+            
+
+
+        }
+
+        static int Multiply(int a ,int b ,int c)
+        {
+
+            int result = a * b * c;
+            Console.WriteLine(result);
+            return result;
+        
+        }
+
+
+    }
+}
---
+++ method/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Sentance : ");
+
+            string str = Console.ReadLine();
+            int wordcount = Worrd(str);
+
+            Console.WriteLine("There are " + wordcount + "Words in the sentence" );
+
+
+           
+        }
+
+        static int Worrd(string str)
+        {
+            int wordcount = str.Split(' ').Length;
+            return wordcount;
+        
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+       /*static void MeetAlien()
+        {
+            Random numbergen = new Random();
+            string name = "X-" + numbergen.Next(10, 99999);
+
+            int age = numbergen.Next(10, 500);
+
+            Console.WriteLine("HI i am \n" + name  + "My age is \n "  + age);
+            Console.WriteLine("oh and i am an alien");
+        
+        }*/
+
+    }
+}
